29f41fe18d2f998c885915813055622ff1b7aeaa
Peter Driscoll <peter.driscoll59@gmail.com>
2020-03-17 20:40:42 +1100

    Put the parser tests in their own file. Plan to break the tests into groups for easier navigation..

--- test_sn/testsn_cppunitentrypoint.cpp
+++
@@ -1,6 +0,0 @@
-#include "cppunit/nova_extensions/cppunitentrypoint.cpp"
-// Command line for debugging on windows:
-// d:\tools\cppunit\lib\dllplugintester_dll.exe 
-//
-// Program arguments for debugging:
-// -w -b c:\nova\lib\TestALSNGL_plugin.dll 
