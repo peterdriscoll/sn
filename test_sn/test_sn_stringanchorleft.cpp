96c0dd1f7beafe4fd50b10df5e71a11368811cd9
Peter Driscoll <peter.driscoll59@gmail.com>
2025-08-01 10:25:51 +1000

    Split anchor tests into LeftAnchorTests and RightAnchorTests (3 each). Added 2 new test suites, total remains 132 tests. All passing.

---
+++ test_sn/test_sn_stringanchorleft.cpp
@@ -0,0 +1,178 @@
+#include "snl.h"
+
+#include <cstdio>
+#include <fstream>
+
+#include "test_sn_pch.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace std;
+using namespace skynet;
+using namespace PGCX;
+
+namespace test_sn
+{
+	TEST_CLASS(test_sn_stringanchorleft)
+	{
+	private:
+		bool runWebServer = false;
+
+		static void AssertErrorHandler(bool p_Err, const string& p_Description)
+		{
+			wstring errDesciption(p_Description.begin(), p_Description.end());
+			Assert::IsTrue(!p_Err, errDesciption.c_str());
+		}
+
+		void Initialize()
+		{
+			Manager::LogicSetup();
+			Transaction::ResetNetMemoryUsed();
+			Transaction::ResetGrossMemoryUsed();
+
+			Thread thread;
+			if (thread.HasTopManager())
+			{
+				Assert::IsTrue(!thread.HasTopManager());
+			}
+		}
+
+		void Cleanup()
+		{
+			Thread thread;
+			if (thread.HasTopManager())
+			{
+				Assert::IsTrue(!thread.HasTopManager());
+			}
+			if (Transaction::TotalNetMemoryUsed() != 0)
+			{
+				Assert::IsTrue(Transaction::TotalNetMemoryUsed() == 0);
+			}
+			if (Promotion::PromotionUsedMemory() != 0)
+			{
+				Assert::IsTrue(Promotion::PromotionUsedMemory() == 0);
+			}
+			if (Promotion::PromotionFreeMemory() != Transaction::TotalGrossMemoryUsed())
+			{
+				Assert::IsTrue(Promotion::PromotionFreeMemory() == Transaction::TotalGrossMemoryUsed());
+			}
+		}
+
+	public:
+		TEST_METHOD(TestLeftAnchoredStringRefPositive)
+		{
+			Initialize();
+			{
+				Manager manager("Test Left-Anchored Positive", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(end);
+
+					// Known start, unknown end: left-anchored StringRef
+					StringRef ref(String("abcdef"), Long(0), end);
+
+					// Assert that the anchored slice equals "ab"
+					(String("ab") == ref).Assert().Do();
+					
+					Long end_Long = end.GetSafeValue();
+					long end_number = end_Long.GetNumber();
+					std::wstring msg = std::wstring(L"End should be 2. Found ") + ToString(end_number);
+					Assert::IsTrue(end_number == 2, msg.c_str());
+
+					// Now end should have been resolved to 2
+					(end == Long(2)).Assert().Do();
+				}
+			}
+			Cleanup();
+		}
+		TEST_METHOD(TestLeftAnchoredStringRefMismatch)
+		{
+			Initialize();
+			{
+				Manager manager("Test Left-Anchored Mismatch", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(end);
+
+					// Known start, unknown end: left-anchored StringRef
+					StringRef ref(String("abcdef"), Long(0), end);
+
+					// Attempt to match "zz" (which doesn't match the source "abcdef")
+					Error err = (String("zz") == ref).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					std::wstring msg = std::wstring(L"Expected mismatch error. Found: ") + ToString(errDescription.c_str());
+					Assert::IsTrue(err.IsError(), msg.c_str());
+
+					// Optionally check that the error string contains the mismatch detail
+					std::string errDesc = err.GetDescription();
+					Assert::IsTrue(errDesc.find("Contradiction: Strings do not match") != std::string::npos,
+						L"Error should describe the mismatch");
+				}
+			}
+			Cleanup();
+		}
+
+		TEST_METHOD(TestLeftAnchoredStringRefNegation)
+		{
+			Initialize();
+			{
+				Manager manager("Test Left-Anchored Negation", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(end);
+
+					// Compare a fixed string to a left-anchored stringref with unknown end
+					(!(String("ab") == StringRef(String("abcdef"), Long(0), end))).Assert().Do();
+
+					// Asserting end == 2 forces:
+					//   String("ab") == StringRef(String("abcdef"), Long(0), end)
+					// This must fail because the delayed inequality will be violated.
+					Error err = (end == Long(2)).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					Assert::IsTrue(err.IsError());
+				}
+				{
+					Transaction transaction;
+
+					SN_DECLARE(end);
+
+					// Compare a fixed string to a left-anchored stringref with unknown end
+					((String("ab") != StringRef(String("abcdef"), Long(0), end))).Assert().Do();
+
+					// Asserting end == 2 forces:
+					//   String("ab") == StringRef(String("abcdef"), Long(0), end)
+					// This must fail because the delayed inequality will be violated.
+					Error err = (end == Long(2)).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					Assert::IsTrue(err.IsError());
+				}
+				{
+					Transaction transaction;
+
+					SN_DECLARE(end);
+
+					// Compare a fixed string to a left-anchored stringref with unknown end
+					(StringRef(String("abcdef"), Long(0), end) != String("ab")).Assert().Do();
+
+					// Asserting end == 2 forces:
+					//   String("ab") == StringRef(String("abcdef"), Long(0), end)
+					// This must fail because the delayed inequality will be violated.
+					Error err = (end == Long(2)).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					Assert::IsTrue(err.IsError());
+				}
+			}
+			Cleanup();
+		}
+	};
+}
---
+++ test_sn/test_sn_stringanchorright.cpp
@@ -0,0 +1,147 @@
+#include "snl.h"
+
+#include <cstdio>
+#include <fstream>
+
+#include "test_sn_pch.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace std;
+using namespace skynet;
+using namespace PGCX;
+
+namespace test_sn
+{
+	TEST_CLASS(test_sn_stringanchorright)
+	{
+	private:
+		bool runWebServer = false;
+
+		static void AssertErrorHandler(bool p_Err, const string& p_Description)
+		{
+			wstring errDesciption(p_Description.begin(), p_Description.end());
+			Assert::IsTrue(!p_Err, errDesciption.c_str());
+		}
+
+		void Initialize()
+		{
+			Manager::LogicSetup();
+			Transaction::ResetNetMemoryUsed();
+			Transaction::ResetGrossMemoryUsed();
+
+			Thread thread;
+			if (thread.HasTopManager())
+			{
+				Assert::IsTrue(!thread.HasTopManager());
+			}
+		}
+
+		void Cleanup()
+		{
+			Thread thread;
+			if (thread.HasTopManager())
+			{
+				Assert::IsTrue(!thread.HasTopManager());
+			}
+			if (Transaction::TotalNetMemoryUsed() != 0)
+			{
+				Assert::IsTrue(Transaction::TotalNetMemoryUsed() == 0);
+			}
+			if (Promotion::PromotionUsedMemory() != 0)
+			{
+				Assert::IsTrue(Promotion::PromotionUsedMemory() == 0);
+			}
+			if (Promotion::PromotionFreeMemory() != Transaction::TotalGrossMemoryUsed())
+			{
+				Assert::IsTrue(Promotion::PromotionFreeMemory() == Transaction::TotalGrossMemoryUsed());
+			}
+		}
+
+	public:
+		TEST_METHOD(TestRightAnchoredStringRefPositive)
+		{
+			Initialize();
+			{
+				Manager manager("Test Right-Anchored Positive", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(start);
+
+					// Known end, unknown start: right-anchored StringRef
+					StringRef ref(String("abcdef"), start, Long(6));
+
+					// Assert that the anchored slice equals "ef"
+					(String("ef") == ref).Assert().Do();
+
+					Long start_Long = start.GetSafeValue();
+					long start_number = start_Long.GetNumber();
+					std::wstring msg = std::wstring(L"Start should be 4. Found ") + ToString(start_number);
+					Assert::IsTrue(start_number == 4, msg.c_str());
+
+					// Now start should have been resolved to 4
+					(start == Long(4)).Assert().Do();
+				}
+			}
+			Cleanup();
+		}
+
+		TEST_METHOD(TestRightAnchoredStringRefMismatch)
+		{
+			Initialize();
+			{
+				Manager manager("Test Right-Anchored Mismatch", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(start);
+
+					// Known end, unknown start: right-anchored StringRef
+					StringRef ref(String("abcdef"), start, Long(6));
+
+					// Attempt to match "zz" (which doesn't match the source "abcdef")
+					Error err = (String("zz") == ref).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					std::wstring msg = std::wstring(L"Expected mismatch error. Found: ") + ToString(errDescription.c_str());
+					Assert::IsTrue(err.IsError(), msg.c_str());
+
+					// Check that the error string contains the mismatch detail
+					Assert::IsTrue(
+						errDescription.find("Contradiction: Strings do not match") != std::string::npos,
+						L"Error should describe the mismatch");
+				}
+			}
+			Cleanup();
+		}
+
+		TEST_METHOD(TestRightAnchoredStringRefNegation)
+		{
+			Initialize();
+			{
+				Manager manager("Test Right-Anchored Negation", AssertErrorHandler);
+				manager.StartWebServer(skynet::StepInto, "0.0.0.0", "80", doc_root, runWebServer);
+				{
+					Transaction transaction;
+
+					SN_DECLARE(start);
+
+					// Compare a fixed string to a left-anchored stringref with unknown end
+					((String("ef") != StringRef(String("abcdef"), start, Long(6)))).Assert().Do();
+
+					// Asserting start == 4 forces:
+					//   String("ef") == StringRef(String("abcdef"), start, Long(6))
+					// This must fail because the delayed inequality will be violated.
+					Error err = (start == Long(4)).Assert().DoReturnError();
+
+					string errDescription = err.GetDescription();
+					Assert::IsTrue(err.IsError());
+				}
+			}
+			Cleanup();
+		}
+	};
+}
